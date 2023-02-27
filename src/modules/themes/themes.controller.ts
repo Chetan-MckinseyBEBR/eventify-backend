a0dd11f897b841bba3f71d112997b0dae1ef178f
Chetan Bhat <Chetan_Bhat@mckinsey.com>
2023-02-27 14:42:59 +0530

    controller endpoints added

--- src/modules/themes/themes.controller.ts
+++ src/modules/themes/themes.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UpdateActiveTheme } from './themes.dto';
 
 @Controller('themes')
-export class ThemesController {}
+export class ThemesController {
+  @Get()
+  async findAll() {
+    return 'Return all themes';
+  }
+
+  @Put()
+  @UsePipes(new ValidationPipe({ transform: true }))
+  @HttpCode(HttpStatus.OK)
+  updateActiveTheme(@Body() body: UpdateActiveTheme) {
+    return 'Updated' + JSON.stringify(body);
+  }
+}
